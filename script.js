778c19ed536c981926c969d34e386e0a68c6e3b4
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-08-17 21:47:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount = new Map(); 
+
+	for(const char of str){
+		if(charCount.has(char)){
+			charCount.set(char, charCount.get(char)+1)
+		}
+		else{
+			charCount.set(char,1)
+		}
+	}
+
+	for(const char of str){
+		if(charCount.get(char) === 1){
+			return char;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
+const input = prompt("Enter a string");  
 alert(firstNonRepeatedChar(input)); 
